4a06a532a43215cba34bca02ddb5fe8deadac096
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-01 09:00:41 +0200

    modified quote component files and app component.html file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1, author:'"Start writing, no matter what. The water does not flow until the faucet is turned on."'},
+    {id:2,author:'"Get it down. Take chances. It may be bad, but it\'s the only way you can do anything really good."'},
+    {id:3,author:'"The first draft is just you telling yourself the story."'},
+    {id:4,author:'"You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it."'},
+    {id:5,author:'"Start before you’re ready."'},
+    {id:6,author:'"The first draft is just you telling yourself the story."'},
+  ];
 
   constructor() { }
 
