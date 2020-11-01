27c6c72174852870bfcb51919bfb6fc9c045bfb6
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-01 09:18:39 +0200

    added quote-detail component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,5 @@
 export class Quote {
     id: number;
     saying: string;
+    author:string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, saying:'"Start writing, no matter what. The water does not flow until the faucet is turned on."'},
-    {id:2,saying:'"Get it down. Take chances. It may be bad, but it\'s the only way you can do anything really good."'},
-    {id:3,saying:'"The first draft is just you telling yourself the story."'},
-    {id:4,saying:'"You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it."'},
-    {id:5,saying:'"Start before you’re ready."'},
-    {id:6,saying:'"The first draft is just you telling yourself the story."'},
+    {id:1, saying:'"Start writing, no matter what. The water does not flow until the faucet is turned on."' ,author:'- Louis L’Amour'},
+    {id:2,saying:'"Get it down. Take chances. It may be bad, but it\'s the only way you can do anything really good."', author:'- William Faulkner'},
+    {id:3,saying:'"The first draft is just you telling yourself the story."' , author:'- Terry Pratchett'},
+    {id:4,saying:'"You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it."',author:'- Octavia E. Butler'},
+    {id:5,saying:'"Start before you’re ready."', author:'- Steven Pressfield'},
+    {id:6,saying:'"You can always edit a bad page. You can’t edit a blank page"', author:' - Jodi Picoult'},
   ];
 
   constructor() { }
